1c108da0c9ab7704b993d19837ca785fb4d68dc0
Watson2396 <jwbama413@gmail.com>
2025-02-24 18:01:54 -0500

    feat: initial http server setup

--- main.go
+++ main.go
@@ -1,22 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("hello world")
-
-	godotenv.Load(".env")
+	godotenv.Overload(".env")
 
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("Port is not found in env")
 	}
 
-	fmt.Println("Port:", portString)
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port: %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal("We dead bois: ", err)
+	}
 }
