3d816cc0be7c9c82e46690df4725ce0d7c03dad3
Watson2396 <jwbama413@gmail.com>
2025-02-27 21:00:06 -0500

    feat: adding router and router handlers

---
+++ json.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Println("The server aint feeling good, responding with 500 level error")
+	}
+
+	type errReponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJSON(w, code, errReponse{
+		Error: msg,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Println("Somethin is funky with the payload, bailin out")
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
